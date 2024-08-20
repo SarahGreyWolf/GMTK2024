c98ba571d4db9585a4b3593d51505ce927f30bf2
SarahGreyWolf <m.sarahgreywolf@outlook.com>
2024-08-20 16:51:27 +0100

    fix: make `mod bar` pub

--- rust/src/classes/graphs/mod.rs
+++ rust/src/classes/graphs/mod.rs
@@ -1,7 +1,7 @@
 use godot::classes::{INode2D, Node2D};
 use godot::prelude::*;
 
-mod bar;
+pub mod bar;
 mod point;
 
 #[derive(GodotClass)]
@@ -12,11 +12,7 @@ pub struct Graph {
 
 #[godot_api]
 impl INode2D for Graph {
-    fn init(base: Base<Node2D>) -> Self {
-        godot_print!("I am a Graph");
-
-        Self { base }
-    }
+    fn init(base: Base<Node2D>) -> Self { Self { base } }
 
     fn ready(&mut self) {}
 }
